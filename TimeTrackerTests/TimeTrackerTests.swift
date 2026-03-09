6e9217dfe6798ab4a0cfc40f522b59e4eeb968e3
eugenechantk <me@eugenechantk.me>
2026-03-09 23:35:54 +0800

    Initial Commit

---
+++ TimeTrackerTests/TimeTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeTrackerTests.swift
+//  TimeTrackerTests
+//
+//  Created by Eugene Chan on 3/9/26.
+//
+
+import Testing
+
+struct TimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
